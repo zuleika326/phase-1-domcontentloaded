8649089089f89a77d153d7b37b54352fe4f5aef9
zuleika <njokizuleika@gmail.com>
2023-04-04 12:36:56 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  });
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
